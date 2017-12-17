6dc925c594fe2e379023dc16850220db9ab66f35
Shajid Issa <shajidissa@Shajids-MacBook-Pro.local>
2017-12-17 03:37:01 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+http://localhost:8761
+
+http://www.h2ss.co.uk:8761
---
+++ src/main/java/uk/co/h2ss/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package uk.co.h2ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
